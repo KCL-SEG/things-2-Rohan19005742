6500bde539647776709684b86f533f214c321eb8
Rohan19005742 <60757115+Rohan19005742@users.noreply.github.com>
2022-12-07 22:01:04 +0000

    created form and tests

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
 
+from things.forms import Thing
+from django import forms
+
 # Create your forms here.
+
+class SignUpForm(forms.ModelForm):  # shows what needs to be displayed on the Signup page
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity', 'created_at']
+
+    
